4dd6821733ba050859c931da267fc1486ce07cf2
gavluk <gavluk@teamharbour.com>
2019-07-21 13:39:06 +0300

    Init empty project and modules structure

---
+++ services/src/test/java/ua/com/gavluk/referent/ResouceServiceTest.java
@@ -0,0 +1,14 @@
+package ua.com.gavluk.referent;
+
+import org.junit.jupiter.api.Test;
+
+public class ResouceServiceTest {
+
+
+    @Test
+    void test_resource_lifecycle() {
+
+        // TODO: here is the general resource service test
+    }
+
+}
